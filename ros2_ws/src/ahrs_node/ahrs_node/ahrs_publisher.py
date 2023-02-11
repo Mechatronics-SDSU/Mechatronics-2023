129e26fe5e431a577e45b38b5e78f687d0677a2a
ConnerS <conner.sommmerfield@gmail.com>
2023-02-11 11:05:55 -0800

    Edited AHRS Formatting

--- ros2_ws/src/ahrs_node/ahrs_node/ahrs_publisher.py
+++ ros2_ws/src/ahrs_node/ahrs_node/ahrs_publisher.py
@@ -42,38 +42,46 @@ class AHRS_Node(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.ahrs = SpartonAHRSDataPackets()
         
-    
+        
     def format_orientation(yaw, pitch, roll):
-        yawFormatted = yaw
-        pitchFormatted = pitch
-        rollFormatted = roll
+    '''
+    Yaw, pitch and roll are all returned as NoneType, must convert to str and then to float
+        We can use a formatted float to determine how many decimal places of accuracy we want
+        If you want to change precision change all the 2's to whatever precision you want
+    '''
+        yaw_formatted = yaw
+        pitch_formatted = pitch
+        roll_formatted = roll
 
-        if yawFormatted != None:
-            yawFormatted = round(float(str(yawFormatted)), 2)
-        if pitchFormatted != None:
-            pitchFormatted = round(float(str(pitchFormatted)), 2)
-        if rollFormatted != None:
-            rollFormatted = round(float(str(rollFormatted)), 2)
+        if yaw_formatted != None:
+            yaw_formatted = round(float(str(yaw_formatted)), 2)
+        if pitch_formatted != None:
+            pitch_formatted = round(float(str(pitch_formatted)), 2)
+        if roll_formatted != None:
+            roll_formatted = round(float(str(roll_formatted)), 2)
             
-        return yawFormatted, pitchFormatted, rollFormatted
+        return yaw_formatted, pitch_formatted, roll_formatted
 
 
     def timer_callback(self):
         yaw = self.ahrs.get_true_heading()
         pitch, roll = self.ahrs.get_pitch_roll()
-
-        '''Yaw, pitch and roll are all returned as NoneType, must convert to str and then to float
-        We can use a formatted float to determine how many decimal places of accuracy we want
-        If you want to change precision change all the 2's to whatever precision you want'''
-
+        
         yaw, pitch, roll = format_orientation(yaw, pitch, roll)
 
-        '''Now that we have our data types converted, we can throw the floats into an array which is the format needed for our custom position (ToDo: Change this to orientation) message. Then we publish our message using the formatted string below'''
+        '''
+        Now that we have our data types converted, we can throw the floats into an array which is the format needed for our custom position
+        (ToDo: Change this to orientation) message. Then we publish our message using the formatted string below
+        '''
         if (yaw != None and pitch != None and roll != None):
             msg = Position()
             msg.position = [yaw, pitch, roll]
             self.publisher_.publish(msg)
-            self.get_logger().info('Publishing Orientation Data: "\nyaw: %f\npitch: %f\nroll: %f\n"' % (msg.position[0], msg.position[1], msg.position[2]))
+            self.get_logger().info
+            (
+                'Publishing Orientation Data: "\nyaw: %f\npitch: %f\nroll: %f\n"' % 
+                (msg.position[0], msg.position[1], msg.position[2])
+            )
 
 
 def main(args=None):
