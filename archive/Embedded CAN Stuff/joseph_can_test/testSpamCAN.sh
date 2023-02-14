c907f3344c158ce889776b0e888876a535a1b673
SDSU-Mechatronics <32554756+SDSU-Mechatronics@users.noreply.github.com>
2023-02-13 23:48:27 -0800

    fixed oopsie. dont commit to master

--- archive/Embedded CAN Stuff/joseph_can_test/testSpamCAN.sh
+++
@@ -1,3 +0,0 @@
-cansend can0 000#12301230
-cansend can0 000#DEADBEEF
-cansend can0 000#ABCABCAB
--- scripts/Embedded CAN Stuff/canTests0/speedrun.sh
+++
@@ -1,5 +0,0 @@
-cansend can0 000#
-cansend can0 011#40C0
-sleep 1.5
-cansend can0 011#0000
-cansend can0 000#
