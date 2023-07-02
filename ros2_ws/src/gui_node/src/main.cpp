b33c2d69c534584012819671eab94bd91ab40845
Your Name <>
2023-07-02 02:16:56 -0700

    Lost 3 More Years Getting Ros Thread and QT Thread Running at Same Time + Stylesheet

--- ros2_ws/src/gui_node/src/main.cpp
+++ ros2_ws/src/gui_node/src/main.cpp
@@ -1,11 +1,41 @@
-#include "mainwindow.h"
-
+#include <QFile>
 #include <QApplication>
 
-int main(int argc, char *argv[])
+#include "rclcpp/rclcpp.hpp"
+#include "mainwindow.h"
+
+class GUI : public rclcpp::Node
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    public:
+        GUI()
+        : Node("gui_node")
+        {
+            
+        }
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<GUI>();
+
+    std::thread rosThread([&]() {
+        rclcpp::executors::MultiThreadedExecutor executor;
+        executor.add_node(node);
+        executor.spin();
+    });
+
+    QApplication app(argc, argv);
+    QFile styleFile("/home/mechatronics/test/ros2_ws/src/gui_node/styles.qss");
+    styleFile.open(QFile::ReadOnly);
+    QString style(styleFile.readAll());
+    app.setStyleSheet(style);
+    MainWindow window;
+
+    window.show();
+    app.exec();
+
+    rosThread.join();
+    rclcpp::shutdown();
+    return 0;
 }
