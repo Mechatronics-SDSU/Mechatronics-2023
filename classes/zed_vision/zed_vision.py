1c586676ad7fd758e94f2bc60a2d9916a7fc02d9
Joshua Van Doren <72502663+javda4@users.noreply.github.com>
2023-07-20 14:22:16 -0700

    Update zed_vision.py both weights dev-josh

--- classes/zed_vision/zed_vision.py
+++ classes/zed_vision/zed_vision.py
@@ -156,6 +156,7 @@ class Zed_Vision():
     def initCamera(self):
         parser = argparse.ArgumentParser()
         parser.add_argument('--weights', nargs='+', type=str, default='/home/mechatronics/master/classes/zed_vision/new_gate.pt', help='model.pt path(s)') #where we put weights at
+        #parser.add_argument('--weights', nargs='+', type=str, default='/home/mechatronics/master/classes/zed_vision/underwater_m.pt', help='model.pt path(s)')
         parser.add_argument('--svo', type=str, default=None, help='optional svo file')
         parser.add_argument('--img_size', type=int, default=640, help='inference size (pixels)')
         parser.add_argument('--conf_thres', type=float, default=0.4, help='object confidence threshold')
